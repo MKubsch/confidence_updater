362958739e54b2d9110c9732a419bd538ce51098
MKubsch <astronomicon@me.com>
2021-08-27 17:02:42 +0200

    Add files via upload
    Some wording changes.

--- app.R
+++ app.R
@@ -23,16 +23,16 @@ ui <- fluidPage(
                       value = ""),
             
             radioButtons("Conf_init",
-                        "How sure are you about your hypothesis? Use the slider to express how confident your are that your hypothesis is correct!",
+                        "How sure are you about your hypothesis? Choose the option that best fits what you already know!",
                         c("rather sure that it is correct" = 0.75,
                           "No idea!" = 0.5,
                           "rather sure that it is incorrect" = 0.25),
                         selected = character(0)),
         
-            radioButtons("Updating_factor", "Does the evidence support your initial hypothesis? Choose the option that best fits the evidence!" ,
-                        c("strongly supports initial hypothesis" = 2,
+            radioButtons("Updating_factor", "How compatible is the evidence with your hypothesis vs. an alternative hypothesis? Choose the best fitting option!" ,
+                        c("Data strongly favors my hypothesis" = 2,
                           "Evidence not conclusive" = 1,
-                          "strongly rejects initial hypothesis" = 0.5), selected = character(0))
+                          "Data strongly favors an alternative hypothesis" = 0.5), selected = character(0))
             
         ),
         # Show a plot of the generated distribution
@@ -66,7 +66,7 @@ server <- function(input, output) {
     
     output$confidence <- renderText({
         c <- as.numeric(input$Updating_factor)*as.numeric(input$Conf_init) / (as.numeric(input$Updating_factor)*as.numeric(input$Conf_init)+1-as.numeric(input$Conf_init))
-        d <- ifelse(c > 0.5,"can be rather sure that it is correct.",ifelse(c == 0.5,"need more evidence and remain undecided about it.","can be rather sure that it is incorrect."))
+        d <- ifelse(c > 0.6,"can be rather sure that it is correct.",ifelse(c > 0.4,"need more evidence and remain undecided about it.","can be rather sure that it is incorrect."))
         paste("I",d)
     })
 }
