807037b54c3bc6bdc7d0182c9421dde8289ebc49
Joshua Rosenberg <jrosen@msu.edu>
2021-08-29 22:03:12 -0400

    working two tab version

--- app.R
+++ app.R
@@ -1,12 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(shinythemes)
 
@@ -18,10 +9,14 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                 
                 tabsetPanel(id = "maintabset",
                             tabPanel("What I know",
+                                     value = "inputs",
+                                     
+                                     p(),
                                      
                                      textInput(inputId = "Hypothesis",
-                                               label = "My hypothesis:",
-                                               value = ""),
+                                               label = "What is your hypothesis?",
+                                               value = "",
+                                               width = "100%"),
                                      
                                      radioButtons("Conf_init",
                                                   "How sure are you about your hypothesis? Choose the option that best fits what you already know!",
@@ -34,7 +29,8 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                                                     "very that my hypothesis is incorrect" = 0.25,
                                                     "really sure that my hypothesis is incorrect" = 0.1,
                                                     "absolutely certain that my hypothesis is incorrect" = 0),
-                                                  selected = character(0)),
+                                                  selected = character(0),
+                                                  width = "100%"),
                                      
                                      radioButtons("Updating_factor", "How compatible is the evidence with your hypothesis relative to an alternative hypothesis? Choose the best fitting option!" ,
                                                   c("Data strongly favors my hypothesis" = 20,
@@ -43,49 +39,57 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                                                     "Evidence not conclusive" = 1,
                                                     "Data somewhat favors an alternative hypothesis" = 1/3,
                                                     "Data favors an alternative hypothesis" = 1/6,
-                                                    "Data strongly favors an alternative hypothesis" = 1/20), selected = character(0)),
+                                                    "Data strongly favors an alternative hypothesis" = 1/20), 
+                                                  selected = character(0),
+                                                  width = "100%"),
+                                     hr(),
                                      checkboxInput("NumericOutput", "Show numeric confidence level."),
-                                     actionButton("run", "Run!")
+                                     actionButton("button", "Run!")
                             ),
                             tabPanel("Estimated confidence",
-                                     
+                                     value = "estimate",
                                      h4("After considering the evidence for my hypothesis"),
                                      em(h4(textOutput("Hypothesis", container = span))),
                                      h4(textOutput("confidence", container = span))
                                      
                             )
-                )
+                ),
+                p(),
+                hr(),
+                p("This Shiny app is created by Marcus Kubsch with contributions from Joshua Rosenberg, E.J. Wagenmakers, and Mine Dogucu. 
+                A pre-print related to this app is available at https://osf.io/aznyq/.
+                It was inspired in part by Warren's (2018) work (https://doi.org/10.1119/1.5012750).
+                  If you have any questions, you may contact Marcus Kubsch (kubsch@leibniz-ipn.de). 
+                  The code for this app may be found at https://github.com/MKubsch/confidence_updater")
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
     
-    Conf_init_input <- reactive({
-        input$Conf_init
+    observeEvent(input$button, {
+        updateTabsetPanel(session, 
+                          "maintabset",
+                          selected = "estimate")
     })
     
-    update_input <- reactive({
-        input$Updating_factor
-    })
-    
-    output$Hypothesis <- renderText({
-        input$Hypothesis
-    })
-    
-    output$confidence <- renderText({
-        c <- as.numeric(input$Updating_factor)*as.numeric(input$Conf_init) / (as.numeric(input$Updating_factor)*as.numeric(input$Conf_init)+1-as.numeric(input$Conf_init))
-        d <- ifelse(c > 0.9,"can be very sure that it is correct.", ifelse(c > 0.8,"can be sure that it is correct.",
-                                                                           ifelse(c > 0.6,"can be rather sure that it is correct.",
-                                                                                  ifelse(c > 0.4,"need more evidence and remain undecided about it.",
-                                                                                         ifelse(c > 0.2, "can be rather sure that it is incorrect",
-                                                                                                ifelse(c > 0.1, "can be sure that it is incorrect.", "can be very sure that it is incorrect." ))))))
-        if(input$NumericOutput == 1){
-            paste("I",d,"(", round(c*100,2),"% confidence)")
-        } else{
-            paste("I",d)
-        } 
+    observeEvent(input$button, {
         
-    })
+        output$confidence <- renderText({
+            c <- as.numeric(input$Updating_factor)*as.numeric(input$Conf_init) / (as.numeric(input$Updating_factor)*as.numeric(input$Conf_init)+1-as.numeric(input$Conf_init))
+            d <- ifelse(c > 0.9,"can be very sure that it is correct.", ifelse(c > 0.8,"can be sure that it is correct.",
+                                                                               ifelse(c > 0.6,"can be rather sure that it is correct.",
+                                                                                      ifelse(c > 0.4,"need more evidence and remain undecided about it.",
+                                                                                             ifelse(c > 0.2, "can be rather sure that it is incorrect",
+                                                                                                    ifelse(c > 0.1, "can be sure that it is incorrect.", "can be very sure that it is incorrect." ))))))
+            if(input$NumericOutput == 1){
+                paste("I",d,"(", round(c*100,2),"% confidence)")
+            } else{
+                paste("I",d)
+            } 
+            
+        })
+    }
+    )
 }
 
 # Run the application 
